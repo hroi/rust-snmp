fefa0e97831c1f8467b9141dd67eda0af5ffc71d
Hroi Sigurdsson <hroi@asdf.dk>
2017-01-22 20:15:23 +0100

    Add doc example for SyncSession::set().
    Fixes #1.

--- src/lib.rs
+++ src/lib.rs
@@ -41,13 +41,13 @@
 //! use std::time::Duration;
 //! use snmp::{SyncSession, Value};
 //!
-//! let sys_descr_oid = [1,3,6,1,2,1,1,1,];
+//! let sys_descr_oid = &[1,3,6,1,2,1,1,1,];
 //! let agent_addr    = "198.51.100.123:161";
 //! let community     = b"f00b4r";
 //! let timeout       = Duration::from_secs(2);
 //!
 //! let mut sess = SyncSession::new(agent_addr, community, Some(timeout), 0).unwrap();
-//! let mut response = sess.getnext(&sys_descr_oid[..]).unwrap();
+//! let mut response = sess.getnext(sys_descr_oid).unwrap();
 //! if let Some((_oid, Value::OctetString(sys_descr))) = response.varbinds.next() {
 //!     println!("myrouter sysDescr: {}", String::from_utf8_lossy(sys_descr));
 //! }
@@ -57,7 +57,7 @@
 //! use std::time::Duration;
 //! use snmp::SyncSession;
 //!
-//! let system_oid      = [1,3,6,1,2,1,1,];
+//! let system_oid      = &[1,3,6,1,2,1,1,];
 //! let agent_addr      = "[2001:db8:f00:b413::abc]:161";
 //! let community       = b"f00b4r";
 //! let timeout         = Duration::from_secs(2);
@@ -65,8 +65,27 @@
 //! let max_repetitions = 7; // number of items in "system" OID
 //!
 //! let mut sess = SyncSession::new(agent_addr, community, Some(timeout), 0).unwrap();
-//! let response = sess.getbulk(&[&system_oid[..]], non_repeaters, max_repetitions).unwrap();
+//! let response = sess.getbulk(&[system_oid], non_repeaters, max_repetitions).unwrap();
 //!
+//! for (name, val) in response.varbinds {
+//!     println!("{} => {:?}", name, val);
+//! }
+//! ```
+//! ## SET
+//! ```no_run
+//! use std::time::Duration;
+//! use snmp::{SyncSession, Value};
+//!
+//! let syscontact_oid  = &[1,3,6,1,2,1,1,4,0];
+//! let contact = Value::OctetString(b"Thomas A. Anderson");
+//! let agent_addr      = "[2001:db8:f00:b413::abc]:161";
+//! let community       = b"f00b4r";
+//! let timeout         = Duration::from_secs(2);
+//!
+//! let mut sess = SyncSession::new(agent_addr, community, Some(timeout), 0).unwrap();
+//! let response = sess.set(&[(syscontact_oid, contact)]).unwrap();
+//!
+//! assert_eq!(response.error_status, snmp::snmp::ERRSTATUS_NOERROR);
 //! for (name, val) in response.varbinds {
 //!     println!("{} => {:?}", name, val);
 //! }
